60f3638a4e4826c8ccb5538b9da0a91d021ff425
t <tysonpederson95@gmail.com>
2022-09-27 14:05:31 -0500

    Create updateLibrary function, add css styling to cards, include some temp books to help with styling

--- library.js
+++ library.js
@@ -1,23 +1,68 @@
 let myLibrary = [];
 
-function Book() {
+// Constructor for new books
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 }
 
+// Create form for the user to fill out to add a book to the list, then update the page to include the new book
 function addBookToLibrary() {
     let title = prompt("Title?");
     let author = prompt("Author?");
     let pages = prompt("Pages?");
     let read = prompt("Read yet?");
     myLibrary.push(new Book(title, author, pages, read));
+    updateLibrary();
 }
 
-Book.prototype.info = function() {
-    return(`${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`);
+// Function to loop through myLibrary and add all books from this as cards on the page
+function updateLibrary() {
+    // Clear list to prevent duplicating everything when adding a new book
+    const cards = document.querySelector(".cards");
+    while (cards.firstChild) {
+        cards.removeChild(cards.lastChild);
+    }
+    // Loop through entire library and add all books as cards
+    for (let book in myLibrary) {
+        const card = document.createElement("div");
+        for (let property in myLibrary[book]) {
+            let info = document.createElement("p");
+            info.innerText = `${property.charAt(0).toUpperCase() + property.slice(1)}: ${myLibrary[book][property]}`
+            card.appendChild(info);
+        }
+        let deleteButton = document.createElement("button");
+        deleteButton.innerText = "Remove";
+        deleteButton.classList.add("deleteButton")
+        card.appendChild(deleteButton);
+        card.classList.add("card");
+        document.querySelector(".cards").appendChild(card);
+        document.querySelector(".deleteButton").addEventListener("click", removeBookFromLibrary);
+    }
+}
+
+// Delete the selected book from the library
+function removeBookFromLibrary() {
+
 }
 
 document.querySelector("button").addEventListener("click", addBookToLibrary);
 
+
+
+// Temp function to add a default book for setting layout/design
+function tempBook() {
+    let title = "Goodnight Moon";
+    let author = "Margaret Wise Brown";
+    let pages = "32";
+    let read = "yes";
+    myLibrary.push(new Book(title, author, pages, read));
+    updateLibrary();
+}
+
+tempBook();
+tempBook();
+tempBook();
+tempBook();
