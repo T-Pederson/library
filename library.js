9f769b29f325c26fa4402638ee0a6f40440bdf80
t <tysonpederson95@gmail.com>
2022-09-28 10:14:29 -0500

    Add a toggle switch for read status

--- library.js
+++ library.js
@@ -27,28 +27,10 @@ function addBookToLibrary() {
 // Loop through myLibrary and add all books as cards on the page
 function updateLibrary() {
     // Clear list to prevent duplicating everything when adding a new book
-    const cards = document.querySelector(".cards");
-    while (cards.firstChild) {
-        cards.removeChild(cards.lastChild);
-    }
+    clearLibrary();
     // Create card, add content to card, and add card to page
-    let bookCount = 0;
-    for (let book in myLibrary) {
-        const card = document.createElement("div");
-        card.setAttribute("id", bookCount);
-        bookCount++;
-        for (let property in myLibrary[book]) {
-            let info = document.createElement("p");
-            info.innerText = `${property.charAt(0).toUpperCase() + property.slice(1)}: ${myLibrary[book][property]}`
-            card.appendChild(info);
-        }
-        let removeButton = document.createElement("button");
-        removeButton.innerText = "Remove";
-        removeButton.classList.add("removeButton")
-        card.appendChild(removeButton);
-        card.classList.add("card");
-        document.querySelector(".cards").appendChild(card);
-    }
+    createCard();
+    // Make remove buttons work
     document.querySelectorAll(".removeButton").forEach(item => {
         item.addEventListener("click", removeBook);
     })
@@ -64,14 +46,68 @@ window.onclick = (event) => {
     }
 }
 
-// Make remove button work
+// Make remove buttons work
 function removeBook () {
     let bookIndex = this.parentElement.id;
     myLibrary.splice(bookIndex, 1);
     updateLibrary();
 }
 
-// Make the add book button work
+// Helper function to clear library when updating library
+function clearLibrary () {
+    const cards = document.querySelector(".cards");
+    while (cards.firstChild) {
+        cards.removeChild(cards.lastChild);
+    }
+}
+
+// Helper function to create new cards when updating library
+function createCard () {
+    // Start counter to assign index location to each card
+    let bookCount = 0;
+    // Loop through myLibrary and create a card div for each, assign index as id
+    for (let book in myLibrary) {
+        const card = document.createElement("div");
+        card.setAttribute("id", bookCount);
+        card.classList.add("card");
+        bookCount++;
+        // Loop through book properties and add content to the newly created card
+        for (let property in myLibrary[book]) {
+            if (property == "read") {
+                // Create slider to toggle read status
+                let sliderDiv = document.createElement("div");
+                card.appendChild(sliderDiv);
+                let span = document.createElement("span");
+                span.innerText = "Read:";
+                sliderDiv.appendChild(span);
+                let label = document.createElement("label");
+                label.classList.add("switch");
+                sliderDiv.appendChild(label);
+                let readStatus = document.createElement("input");
+                readStatus.type = "checkbox";
+                readStatus.id = "read";
+                label.appendChild(readStatus);
+                let slider = document.createElement("span");
+                slider.classList.add("slider", "round");
+                label.appendChild(slider);
+            } else {
+                // Create text for title, author and page count
+                let info = document.createElement("p");
+                info.innerText = `${property.charAt(0).toUpperCase() + property.slice(1)}: ${myLibrary[book][property]}`
+                card.appendChild(info);
+            }
+        }
+        // Create remove button
+        let removeButton = document.createElement("button");
+        removeButton.innerText = "Remove";
+        removeButton.classList.add("removeButton")
+        card.appendChild(removeButton);
+        // Add card to cards list
+        document.querySelector(".cards").appendChild(card);
+    }
+}
+
+// Make clicking the add book button work
 document.querySelector("#addBook").addEventListener("click", addBookToLibrary);
 
 
